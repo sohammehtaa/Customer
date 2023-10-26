c3b0fc33d76f527ca0838a817766578be99be273
dac <dac@dypm.org>
2023-10-26 09:59:28 +0530

    customer done

--- customer/validation/ValidateCustomer.java
+++ customer/validation/ValidateCustomer.java
@@ -9,20 +9,23 @@ public class ValidateCustomer {
 
 	public boolean validatePlan(String plan)
 	{
-		for(serviceplan s: serviceplan.values())
-			if(s.equals(plan))
+		for(serviceplan s: serviceplan.values()) 
+		{
+			if(s==serviceplan.valueOf(plan.toUpperCase()))
 				return true;
+		}
 		return false;
 	}
 	
-	public boolean validateRegAmount(String plan,Double regAmount )
+	
+	public boolean validateRegAmount(String plan,Double regAmount)
 	{
 		for(serviceplan s: serviceplan.values())
 		{
-			serviceplan sp=serviceplan.DIAMOND;
-			if(sp.getServiceCharges().equals(regAmount))
-		return false;
+			if(s.getServiceCharges()==regAmount)
+				return true;
 		}
+		return false;
 	}
 	
 	public static Customer validateAll(int customerId,String firstName, String lastName, String email, String password, Double regAmount, LocalDate dob, serviceplan plan) throws IllegalArgumentException, CustomException
@@ -31,3 +34,13 @@ public class ValidateCustomer {
 	}
 	
 }
+
+
+
+/*public boolean validatePlan(String plan)
+{
+	for(serviceplan s: serviceplan.values())
+		if(s.equals(plan))
+			return true;
+	return false;
+}*/
